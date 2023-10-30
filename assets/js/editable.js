c8e0ea2b776c14ec7a3c459c9cd916108036942e
Milad pegah <miladpegah@amoozesh-boors.com>
2023-10-30 16:53:40 +0330

    feat!: about us edit action

--- assets/js/editable.js
+++ assets/js/editable.js
@@ -1,3 +1,4 @@
+let aboutItemsTarget = {};
 const loadSelectedImage = (input, target, bg) => {
   if (input.files && input.files[0]) {
     const reader = new FileReader();
@@ -38,4 +39,35 @@ $(document.body).on("change", ".file-input-hidden", (e) => {
   let bg = $(e.target).data("bg") == true ? true : false;
   let name = $(e.target).attr("name");
   loadSelectedImage(changed, target, bg);
+});
+
+$(document.body).on("click", ".editable-about-description-action", (e) => {
+  let id = $(e.target).data("id");
+  let title = $(e.target).data("title");
+  let description = $(e.target).data("description");
+  aboutItemsTarget = {
+    id: id,
+    title: title,
+    description: description,
+  };
+
+  $("#edit_about_item_id").val(id);
+  $("#edit_about_item_title").val($(title).html());
+  $("#edit_about_item_description").val($(description).html());
+  $("#about-items-modal").modal("toggle");
+});
+
+$(document.body).on("submit", ".about-edit-item-form", (e) => {
+  e.preventDefault();
+  let id = $("#edit_about_item_id").val();
+  let title = $("#edit_about_item_title").val();
+  let description = $("#edit_about_item_description").val();
+  let targetRecord = aboutItems.find((item) => {
+    return item.id === aboutItemsTarget.id;
+  });
+  targetRecord.title = title;
+  targetRecord.description = description;
+  $(aboutItemsTarget.title).html(title);
+  $(aboutItemsTarget.description).html(description);
+  $("#about-items-modal").modal("toggle");
 });
